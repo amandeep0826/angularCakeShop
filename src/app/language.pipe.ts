f132bbdf04fe27815daa4fb444355b92e3430686
Amandeep <amandeep.henceforth@gmail.com>
2021-10-22 10:52:37 +0530

    user defined pipe

--- src/app/language.pipe.ts
+++ src/app/language.pipe.ts
@@ -1,14 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+var spanish: any = { "Hello": "Hola", "Good Morning": "Buenos dias" }
+
 @Pipe({
   name: 'language'
 })
 export class LanguagePipe implements PipeTransform {
 
   transform(value: any, ...args: any[]): any {
-    var spanish = {"hello":"hola", "Amandeep's Cake Shop": "pastelería"}
-    value = spanish
-    return value;
+    return spanish[value] || value
   }
 
 }
